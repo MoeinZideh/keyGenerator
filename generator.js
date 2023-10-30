d36bae916334eeda76b94666b146145a847f83a1
zidehsaraee <moein.zideh@gmail.com>
2023-10-30 16:18:06 +0000

    Initial commit

---
+++ generator.js
@@ -0,0 +1,8 @@
+document.addEventListener('keydown', function(event) {
+    const reportDiv = document.getElementById('report');
+    reportDiv.innerHTML = `
+        <p>Code: ${event.code}</p>
+        <p>Key: ${event.key}</p>
+        <p>Which: ${event.which}</p>
+    `;
+});
